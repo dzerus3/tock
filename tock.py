df4b61c32e2811db50d23554d24bee5f2b032ade
Dellon Zerus <dellonzerus@gmail.com>
2020-12-01 14:24:32 -0800

    Added creation of Tkinter progressbars
    Why:
    So we have a reliable way to create them based on preset
    
    How:
    Loops through preset, creates a progressbar and a label for each entry.
    
    Tags:[ui][feat][WIP]

--- tock.py
+++ tock.py
@@ -6,42 +6,67 @@ from tkinter import ttk
 
 class Gui(tk.Tk):
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, args, kwargs)
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry("500x500")
+
+        mainTab = MainFrame(self)
+        mainTab.pack()
+        # tabControl = ttk.Notebook(self)
+        # tabControl.add(mainTab)
+
+        self.mainloop()
 
 class MainFrame(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.loadSetup()
 
+        # Dropdown box to select preset
         n = tk.StringVar()
-        self.presets = ttk.Combobox(window, width = 27, textvariable = n)
-        self.presets['values'] = self.setup.keys()
-        self.presets.current(0)
+        self.presets = ttk.Combobox(self, width = 27, textvariable = n)
 
-        createProgress(self.setup[self.presets.current()])
+        presetNames = list(self.setup.keys())
+        self.presets['values'] = presetNames
+        self.presets.pack()
+
+        currentPreset = self.setup[self.presets.get()] #FIXME
+
+        self.createProgress(currentPreset) #FIXME
 
         startButton = tk.Button(
             self,
             text="Start",
             width = 10,
-            command = self.start()
+            command = lambda: self.start(currentPreset)
         )
         startButton.pack(side="bottom")
 
+    # Creates appropriate ammount of progress bars based on preset
     def createProgress(self, preset):
+        bars = []
         for timer in preset:
-            label = Label(self, text = timer)
+            label = tk.Label(self, text = timer)
             label.pack()
 
-            prog = tk.Progressbar(
-                self, orient=tk.HORIZONTAL,
-                length=100, mode="determinate"
+            barLen = preset[timer]["duration"]
+            prog = ttk.Progressbar(
+                self,
+                orient = tk.HORIZONTAL,
+                length = barLen, #TODO: Set to 100?
+                value = barLen, #TODO: Set to max or set to 0?
+                mode="determinate"
             )
-
             prog.pack(pady = 10)
 
-    def start(self):
-        pass
+            bars.append(prog)
+        return bars
+
+    def start(self, preset, bars):
+        while True:
+            time.sleep(1)
+            for bar in bars:
+                bar["value"] -= 1
+            root.update_idletasks()
 
     def loadSetup(self):
         with open("setup.json", "r") as setupFile:
@@ -50,5 +75,5 @@ class MainFrame(tk.Frame):
 def main():
     gui = Gui()
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
